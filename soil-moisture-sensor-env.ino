f7b252ea8b9753738834a05b2b3c458e15d387f4
Krystian Bochenek <87995539+vBagieta@users.noreply.github.com>
2023-05-21 19:53:04 +0200

    Code optimization

--- soil-moisture-sensor-env.ino
+++ soil-moisture-sensor-env.ino
@@ -3,17 +3,17 @@
 #include <hd44780ioClass/hd44780_I2Cexp.h>
 #include "DHT.h"
 #define DHT11_PIN 2
-DHT dht;
 
+DHT dht;
 hd44780_I2Cexp lcd;
 hd44780_I2Cexp lcd2(0x27);
 
 const int LCD_COLS = 16;
 const int LCD_ROWS = 2;
+const int dryValue = 603;
+const int wetValue = 307;
 
-const int AirValue = 603;
-const int WaterValue = 307;
-int intervals = (AirValue - WaterValue)/3;
+int intervals = (dryValue - wetValue)/3;
 int soilMoistureValue = 0;
 
 void setup()
@@ -40,40 +40,28 @@ void loop() {
   int envHumidity = dht.getHumidity();
   int envTemperature = dht.getTemperature();
 
-  if(soilMoistureValue > WaterValue && soilMoistureValue < (WaterValue + intervals)) {
+  if(soilMoistureValue > wetValue && soilMoistureValue < (wetValue + intervals)) {
 
     lcd.print("Wilgotnosc gleby:");
     lcd.setCursor(0, 1);
     lcd.print("");
-    lcd.print("B.Mokra H");
-    lcd.print(envHumidity);
-    lcd.print("%T");
-    lcd.print(envTemperature);
-    lcd.print("*  ");
+    lcd.print("B.Mokra H" + String(envHumidity) + "%T" + String(envTemperature) + "*  ");
     delay(90);
 
-  } else if(soilMoistureValue > (WaterValue + intervals) && soilMoistureValue < (AirValue - intervals)) {
+  } else if(soilMoistureValue > (wetValue + intervals) && soilMoistureValue < (dryValue - intervals)) {
 
     lcd.print("Wilgotnosc gleby:");
     lcd.setCursor(0, 1);
     lcd.print("");
-    lcd.print("Mokra! H");
-    lcd.print(envHumidity);
-    lcd.print("% T");
-    lcd.print(envTemperature);
-    lcd.print("* ");
+    lcd.print("Mokra! H" + String(envHumidity) + "% T" + String(envTemperature) + "* ");
     delay(90);
 
-  } else if(soilMoistureValue < AirValue && soilMoistureValue > (AirValue - intervals)) {
+  } else if(soilMoistureValue < dryValue && soilMoistureValue > (dryValue - intervals)) {
 
     lcd.print("Wilgotnosc gleby:");
     lcd.setCursor(0, 1);
     lcd.print("");
-    lcd.print("Sucha! H");
-    lcd.print(envHumidity);
-    lcd.print("% T");
-    lcd.print(envTemperature);
-    lcd.print("* ");
+    lcd.print("Sucha! H" + String(envHumidity) + "% T" + String(envTemperature) + "* ");
     delay(90);
   }
 }
