41d4f87522d7300152631d3ab42f03b40ceaf365
Krystian Bochenek <87995539+vBagieta@users.noreply.github.com>
2023-05-21 20:30:25 +0200

    Show voltage instead ADC value

--- soil-moisture-sensor.ino
+++ soil-moisture-sensor.ino
@@ -7,11 +7,12 @@ hd44780_I2Cexp lcd2(0x27);
 
 const int LCD_COLS = 16;
 const int LCD_ROWS = 2;
+
 const int dryValue = 603;
 const int wetValue = 307;
-
 int intervals = (dryValue - wetValue)/3;
-int soilMoistureValue = 0;
+float soilMoistureValue = 0;
+float voltage = 0;
 
 void setup()
 {
@@ -32,29 +33,30 @@ void setup()
 
 void loop() {
   soilMoistureValue = analogRead(A0);
+  voltage = (soilMoistureValue * 5) / 1024;
 
   if(soilMoistureValue > wetValue && soilMoistureValue < (wetValue + intervals)) {
 
-    lcd.print("Wilgotnosc gleby:");
+    lcd.print("Wilgotnosc gleby");
     lcd.setCursor(0, 1);
     lcd.print("");
-    lcd.print("B. mokra! (" + String(soilMoistureValue) + ") ");
+    lcd.print("B. mokra! " + String(voltage) + "V ");
     delay(90);
 
   } else if(soilMoistureValue > (wetValue + intervals) && soilMoistureValue < (dryValue - intervals)) {
 
-    lcd.print("Wilgotnosc gleby:");
+    lcd.print("Wilgotnosc gleby");
     lcd.setCursor(0, 1);
     lcd.print("");
-    lcd.print("Mokra! (" + String(soilMoistureValue) + ")    ");
+    lcd.print("Mokra! " + String(voltage) + "V    ");
     delay(90);
 
   } else if(soilMoistureValue < dryValue && soilMoistureValue > (wetValue - intervals)) {
 
-    lcd.print("Wilgotnosc gleby:");
+    lcd.print("Wilgotnosc gleby");
     lcd.setCursor(0, 1);
     lcd.print("");
-    lcd.print("Sucha! (" + String(soilMoistureValue) + ")    ");
+    lcd.print("Sucha! " + String(voltage) + "V    ");
     delay(90);
   }
 }
